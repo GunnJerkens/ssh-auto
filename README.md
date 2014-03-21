6f5427d1873f066e3f8a2ea9ee1cae841877eb2d
Patrick Shampine <pshampine@gunnjerkens.com>
2014-03-21 11:26:02 -0700

    Restructured added some validation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Populates public ssh keys across a multi user account system and sets appropriate ownership/permissions.
 
+It is expected that this is being run by the root user.
+
 
 ### Usage
 
@@ -9,8 +11,13 @@ Populates public ssh keys across a multi user account system and sets appropriat
 
 2) Add each user account (1 per line) to `properties_config.sample` and save as `properties_config`  
 
-3) Add +x to ssh-auto and execute
+3) Add +x to ssh-auto and execute to perform a dry run
 
 ```
 ./ssh_auto.sh
+```
+
+4) If satistied add the option `go`  
+```
+./ssh_auto.sh go
 ```
--- ssh_auto.sh
+++ ssh_auto.sh
@@ -1,16 +1,40 @@
 #! /bin/bash
 
-propArray=( `cat "properties.config" `)
+if [ ! -f `dirname $0`/authorized_keys ]; then
+  echo "please copy authorized_keys.sample to authorized_keys, and edit"
+  exit 1
+fi
 
-echo "Starting key migration to user accounts"
+if [ ! -f `dirname $0`/accounts_config ]; then
+  echo "please copy accounts_config.sample to accounts_config, and edit"
+  exit 1
+fi
 
-for prop in "${propArray[@]}"
+acctArray=( `cat "accounts_config" `)
+
+echo "Starting key migration to user accounts:"
+echo "---------------"
+
+for acct in "${acctArray[@]}"
 do
-  cp authorized_keys /home/$prop/.ssh/authorized_keys
-  chown $prop:$prop /home/$prop/.ssh/authorized_keys
-  chmod 600 /home/$prop/.ssh/authorized_keys
-  echo "$prop complete!"
+  echo "Configuring $acct ..."
+
+  if [ "$1" != "go" ]; then
+
+    echo "cp authorized_keys /home/$acct/.ssh/authorized_keys"
+    echo "chown $acct:$acct /home/$acct/.ssh/authorized_keys"
+    echo "chmod 600 /home/$acct/.ssh/authorized_keys"
+
+  else
+
+    cp authorized_keys /home/$acct/.ssh/authorized_keys
+    chown $acct:$acct /home/$acct/.ssh/authorized_keys
+    chmod 600 /home/$acct/.ssh/authorized_keys
+
+
+  fi
+  echo "$acct complete!"
+  echo "---------------"
 done
 
 echo "Process complete. You are now free to ssh about the server."
-
